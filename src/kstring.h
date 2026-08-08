232e1db83d9fa55cdba0d705ea5b4c65ddb84e4c
mrinosuke <jihadmd444400@gmail.com>
2026-08-08 18:28:23 +0600

    Delete all files

--- src/kstring.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <stddef.h>
-
-size_t kstrlen(const char* s);
-int kstrcmp(const char* a, const char* b);
-void kstrcpy(char* dst, const char* src);
-bool kstartswith(const char* s, const char* prefix);
--- src/serial.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <stdint.h>
-
-void serial_init();
-void serial_write_char(char c);
-void serial_write(const char* str);
-
-/* Non-blocking: returns true and fills *c if a byte is ready */
-bool serial_poll(char* c);
